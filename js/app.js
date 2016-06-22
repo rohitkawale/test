e6b26393e5a2424c52617bd8a685d1522813bda0
rohit kawale <rohit.kawale02@gmail.com>
2016-06-22 11:51:58 +0530

    crud operations simple project

--- js/app.js
+++ js/app.js
@@ -2,12 +2,32 @@ var app= angular.module('myApp',['daterangepicker']);
 
 app.controller('myCtrl',function($scope){
 $scope.data={};
-$scope.users=[];
+$scope.users=[{
+	'name' : "rohit",
+	'surname' : "kawale"
+},
+{
+	'name' : "rohit2",
+	'surname' : "kawale2"
+}
+];
 $scope.datePicker={};
 $scope.datePicker.date = {
 	startDate: moment().subtract(5, "days"),
     endDate: moment()
 };
+
+$scope.edit= function(u){
+	$scope.data=u;
+
+};
+$scope.save= function(){
+	$scope.data={};
+};
+$scope.delete=function(index){
+	$scope.users.splice(index,1);
+
+}
 
 $scope.adduser= function(){
 	var datePicker=$scope.datePicker.date ;
@@ -18,6 +38,7 @@ $scope.adduser= function(){
 		'surname' : $scope.data.surname
 	};
 	$scope.users.push(user);
+	$scope.data={};
 	console.log($scope.users);
 }
 
